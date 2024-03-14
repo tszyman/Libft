d8ca71e899594bd1865b84e9ca5a8da99ef3faf1
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-03-14 20:57:36 +0100

    Function shortened due to Norm errors

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 20:57:50 by tomek             #+#    #+#             */
-/*   Updated: 2024/03/14 20:41:58 by tomek            ###   ########.fr       */
+/*   Updated: 2024/03/14 20:55:01 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,15 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	size_t			i;
 
 	sign = 1;
+	dst = (unsigned char *)(dest);
+	source = (unsigned char *)(src);
 	if (!dest && !src)
 		return (0);
 	if (dest > src)
 	{
 		sign = -1;
-		dst = (unsigned char *)(dest + n - 1);
-		source = (unsigned char *)(src + n - 1);
-	}
-	else
-	{
-		dst = (unsigned char *)(dest);
-		source = (unsigned char *)(src);
+		dst += n - 1;
+		source += n - 1;
 	}
 	i = 0;
 	while (i < n)
