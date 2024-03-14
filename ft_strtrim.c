cedb490f19f4b687a14530fbe93e4c53b6f21dc3
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-03-14 20:33:02 +0100

    strtrim rewritten from scratch

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tszymans <tszymans@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 19:51:25 by tszymans          #+#    #+#             */
-/*   Updated: 2024/03/13 20:41:40 by tszymans         ###   ########.fr       */
+/*   Updated: 2024/03/14 20:30:40 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,18 @@ char	*ft_strtrim(char const *s1, char const *set);
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*trim;
-	char	*s;
-	char	*s_beg;
-	size_t	s1_len;
-	size_t	s_len;
+	size_t	i;
 
-	s = (char *)s1;
-	s1_len = ft_strlen(s);
-	if (*set == '\0')
-		return (s);
-	while (*set)
-	{
-		while (*s == *set && s < s1 + s1_len)
-		{
-			s++;
-		}
-		set++;
-	}
-	s_beg = s;
-	s_len = ft_strlen(s);
-	s = (char *)s1 + s1_len;
-	while (*set)
-	{
-		while (*s == *set && s > s_beg)
-		{
-			s--;
-		}
-		set++;
-	}
-	s_len = s - s_beg;
-	trim = (char *)calloc((s_len + 1), sizeof(char));
-	if (!trim)
+	if (!s1 || !set)
 		return (0);
-	ft_strlcpy(trim, s_beg, s_len + 1);
-	return (trim);
+	while (*s1 && ft_strchr(set, *s1))
+	{
+		s1++;
+	}
+	i = ft_strlen(s1);
+	while (i && ft_strchr(set, *(s1 + i)))
+	{
+		i--;
+	}
+	return (ft_substr(s1, 0, i + 1));
 }
