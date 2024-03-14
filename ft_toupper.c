61651150c8e42701073dcc80900715d37fb7da19
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-03-14 20:34:32 +0100

    norminette errors cleaned

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 21:44:50 by tomek             #+#    #+#             */
-/*   Updated: 2024/03/04 22:01:42 by tomek            ###   ########.fr       */
+/*   Updated: 2024/03/14 20:33:16 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_toupper(int c);
 
-int ft_toupper(int c)
+int	ft_toupper(int c)
 {
 	if ((c >= 97) && (c <= 122))
-		return (c-32);
+		return (c - 32);
 	return (c);
 }
