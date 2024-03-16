9e6f30c113da12dea17f5349422a694e78efec5e
Tomasz Szymanski <tszymans@student.42.fr>
2024-03-16 15:39:53 +0100

    cosmetic changes

--- testy/test_ft_memcpy.c
+++ testy/test_ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tszymans <tszymans@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 17:13:11 by tszymans          #+#    #+#             */
-/*   Updated: 2024/03/06 17:36:44 by tszymans         ###   ########.fr       */
+/*   Updated: 2024/03/16 15:27:03 by tszymans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,22 @@
 
 int main(void)
 {
-	char str[] = "tomek";
-	char dest[10];
-	char org_dest[10];
+	char	str[] = "Tomek";
+	char	dest[10];
+	char	org_dest[10];
+	char	*d;
+	char	*d_o;
 
 	// memcpy(org_dest, str, 6);
 
 	// ft_memcpy(dest, str, 6);
 
-	printf("my: %s \n", (char *)ft_memcpy(dest, str, ft_strlen(str)+1));
-	printf("orig: %s \n", (char *)memcpy(org_dest, str, ft_strlen(str)+1));
+	d = (char *)ft_memcpy(dest, str, ft_strlen(str)+1);
+	d_o = (char *)memcpy(org_dest, str, ft_strlen(str)+1);
+	printf("my: %s \n", d);
+	printf("orig: %s \n", d_o);
 
-	if (*(char *)memcpy(org_dest, str, ft_strlen(str)+1) == *(char *)memcpy(dest, str, ft_strlen(str)+1))
+	if (ft_strlen(d) == ft_strlen(d_o))
 		printf("Test: OK \n");
 	else
 		printf("Test: NOK \n");
